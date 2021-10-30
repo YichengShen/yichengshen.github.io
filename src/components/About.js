40c6a601dad02aa0398a04717cb43d38898c7b19
YichengShen <jsszsyc@hotmail.com>
2021-10-30 12:01:22 +0800

    load data from csv

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,19 @@ import Grid from "@mui/material/Grid";
 import { grey } from "@mui/material/colors";
 import css from "./About.module.css";
 
-const About = () => {
-  const NUMPHOTOS = 3;
+const About = (props) => {
+  const { about } = props;
+  const info = about[0];
+
   const [TimeStamp, setTimeStamp] = useState(0);
   const [PhotoIdx, setPhotoIdx] = useState(0);
 
-  function handleMouseMove(event) {
+  const handleMouseMove = (event) => {
     if (event.timeStamp - TimeStamp > 1000) {
+      setPhotoIdx(Math.floor(Math.random() * info.num_photos));
       setTimeStamp(event.timeStamp);
-      setPhotoIdx(Math.floor(Math.random() * NUMPHOTOS));
     }
-  }
+  };
 
   return (
     <Box
@@ -57,21 +59,30 @@ const About = () => {
             </div>
           </Grid>
           <Grid item xs={12} sm={7} md={8}>
-            <div className={css.text}>
-              Hi, I'm Yicheng.
-              <br />
-              <br />
-              I graduated from Boston College in 2021 with a BA degree in
-              Computer Science. I work as a CS research assistant. I'm
-              interested in distributed systems, machine learning, edge
-              computing, and vehicular clouds.
-              <br />
-              <br />I am currently applying for a master program.
-            </div>
+            <AboutDescription info={info} />
           </Grid>
         </Grid>
       </Box>
     </Box>
+  );
+};
+
+const AboutDescription = (props) => {
+  return (
+    <div className={css.text}>
+      {Object.entries(props.info).map(([key, value]) => {
+        if (key.includes("line")) {
+          return (
+            <div key={key}>
+              {value}
+              <br />
+              <br />
+            </div>
+          );
+        }
+        return null;
+      })}
+    </div>
   );
 };
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import { readString } from "react-papaparse";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import About from "./About";
 
 const App = () => {
+  const [AboutData, setAboutData] = useState(null);
+
+  useEffect(() => {
+    const getData = async (path) => {
+      const response = await fetch(path);
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw array
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value); // the csv string
+      const results = readString(csv, { header: true }); // Use react-papaparse to get the object with { data, errors, meta }
+      const rows = results.data; // array of objects
+      console.log("Data loaded from " + path + ": ");
+      console.log(rows);
+      setAboutData(rows);
+    };
+    getData(process.env.PUBLIC_URL + "/files/about.csv");
+  }, []);
+
   return (
     <>
       <Navbar />
       <Header />
-      <About />
+      {AboutData && <About about={AboutData} />}
     </>
   );
 };
